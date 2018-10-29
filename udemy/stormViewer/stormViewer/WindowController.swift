47e214a7e9a4d3d34bd4fa5f176ed1165c4bbff2
leozhao0709 <zhao434@usc.edu>
2018-10-29 09:35:17 -0700

    inital

---
+++ udemy/stormViewer/stormViewer/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  stormViewer
+//
+//  Created by Lei Zhao on 10/28/18.
+//  Copyright © 2018 Lei Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
