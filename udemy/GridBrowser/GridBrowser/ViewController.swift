7e6f925ebd15289d9466a47de594d4aafbcd3c34
lzhao <zhao434@usc.edu>
2018-10-31 18:47:29 -0700

    gridBrowser project start

---
+++ udemy/GridBrowser/GridBrowser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GridBrowser
+//
+//  Created by Lei Zhao on 10/31/18.
+//  Copyright © 2018 Lei Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
