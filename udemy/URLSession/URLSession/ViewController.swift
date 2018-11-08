f2596e6a01e08d9f3cc99fc16f5705c58865ba45
Lei Zhao <zhao434@usc.edu>
2018-11-07 21:34:09 -0800

    2018-11-07 21:33:37 auto check in

--- udemy/URLSession/URLSession/ViewController.swift
+++ udemy/URLSession/URLSession/ViewController.swift
@@ -8,63 +8,90 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, URLSessionDownloadDelegate {
+    
+    lazy var fileData: Data = {
+        return Data()
+    }()
+    
+    private var session: URLSession?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
     
     @IBAction func clickMe(_ sender: NSButton) {
-//        self.get()
-//        self.objToJSON()
-        self.post()
-    }
-    
-    private func objToJSON() {
-        let post = Post(user_id: 12, id: 10, title: "haha", body:"I'm body")
-        let json = try! JSONEncoder().encode(post)
-        print(String(data: json, encoding: String.Encoding.utf8)!)
-    }
-    
-    private func get() {
-        let session = URLSession.shared
+//        self.download()
         
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/5") else { return }
-        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, err: Error?) in
-            if err == nil {
-                let post = try! JSONDecoder().decode(Post.self, from: data!)
-                print(post)
+        self.delegateDownload()
+    }
+    
+    private func delegateDownload() {
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.main)
+        let url = URL(string: "https://d3cbihxaqsuq0s.cloudfront.net/images/53402047_xl.jpg")!
+        let dataTask = session.downloadTask(with: url)
+        dataTask.resume()
+        self.session = session
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        print("\(CGFloat(totalBytesWritten)/CGFloat(totalBytesExpectedToWrite))")
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
+        print(#function)
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print(#function)
+        
+        print("\(location)-----")
+        
+        let downloadDir = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)[0]
+        let storeUrl = downloadDir.appendingPathComponent((downloadTask.response?.suggestedFilename)!)
+        
+        do {
+            try FileManager.default.moveItem(at: location, to: storeUrl)
+        }
+        catch {
+            print("move error")
+        }
+        
+        print(storeUrl)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        print(#function)
+    }
+    
+    deinit {
+        URLSession.shared.finishTasksAndInvalidate()
+        self.session?.finishTasksAndInvalidate()
+    }
+    
+    
+    private func download() {
+        let session = URLSession.shared
+        let url = URL(string: "https://d3cbihxaqsuq0s.cloudfront.net/images/53402047_xl.jpg")!
+        let downloadTask = session.downloadTask(with: url) { (location: URL?, response: URLResponse?, error: Error?) in
+            print("\(location!)-----")
+            
+            let downloadDir = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)[0]
+            let storeUrl = downloadDir.appendingPathComponent((response?.suggestedFilename)!)
+            
+            do {
+                try FileManager.default.moveItem(at: location!, to: storeUrl)
             }
+            catch {
+                print("move error")
+            }
+            
+            print(storeUrl)
         }
-        dataTask.resume()
+        downloadTask.resume()
     }
-    
-    private func post() {
-        let session = URLSession.shared
-        
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        urlRequest.allHTTPHeaderFields = ["Content-Type": "application/json"]
-        let post = Post(user_id: 12, id: nil, title: "haha", body:"I'm body")
-        urlRequest.httpBody = try? JSONEncoder().encode(post)
-        let dataTask = session.dataTask(with: urlRequest) { (data: Data?, response: URLResponse?, error: Error?) in
-            let post = try? JSONDecoder().decode(Post.self, from: data!)
-            print(post!)
-        }
-        
-        dataTask.resume()
-    }
-    
-
 
 }
 
